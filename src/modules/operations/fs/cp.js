8f9a8f44b5a4d1fe9a1a1baf6c99a3fedaacd83c
Andrii Hordov <andriihordov18@gmail.com>
2025-11-02 16:07:15 +0100

    refactor: refactor cp operation

--- src/modules/operations/fs/cp.js
+++ src/modules/operations/fs/cp.js
@@ -1,18 +1,22 @@
 import { createReadStream, createWriteStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
 import { getModuleName } from '../../../utils/helpers.js';
-import { resolve } from 'node:path';
+import { resolve, parse } from 'node:path';
 import { getItem } from '../../components/storage.js';
 import { access } from 'node:fs/promises';
 
 const operationName = getModuleName(import.meta.filename);
 
-const cp = async (source, destination) => {
+const cp = async (pathToFile, pathToNewDirectory) => {
   const cwd = getItem('cwd');
-  const absoluteSourcePath = resolve(cwd, source);
-  const absoluteDestinationPath = resolve(cwd, destination);
+  const absoluteSourcePath = resolve(cwd, pathToFile);
   await access(absoluteSourcePath);
-  await pipeline(createReadStream(absoluteSourcePath), createWriteStream(absoluteDestinationPath, { flags: 'wx' }));
+  const { name, ext } = parse(absoluteSourcePath);
+  const fileName = `${name}${ext ?? ''}`;
+  const absoluteDestinationPath = resolve(cwd, pathToNewDirectory, fileName);
+  if (absoluteDestinationPath !== absoluteSourcePath) {
+    await pipeline(createReadStream(absoluteSourcePath), createWriteStream(absoluteDestinationPath, { flags: 'wx' }));
+  }
 };
 
 export default {
